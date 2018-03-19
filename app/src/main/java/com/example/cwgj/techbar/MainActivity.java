6514b04cca4311b181f3eb99105d292c8e81aaa2
lidingwei <123456>
2018-03-19 18:14:11 +0800

    use other progject

--- app/src/main/java/com/example/cwgj/techbar/MainActivity.java
+++ app/src/main/java/com/example/cwgj/techbar/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.cwgj.techbar;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.example.cwgj.library.BarUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statusbar_white_toolbar);
-        BarUtil.setStatusBarLightMode(this, Color.WHITE);
+//        BarUtil.setStatusBarLightMode(this, Color.WHITE);
 //        BarUtil.setStatusBarColor(this, ContextCompat.getColor(this, R.color.colorAccent));
     }
 }
